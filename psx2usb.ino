9055514f999a71a905a1dd27fa32f761311d5784
jtryba <jtryba2010@gmail.com>
2019-09-24 12:29:38 -0500

    Update psx2usb.ino

--- psx2usb.ino
+++ psx2usb.ino
@@ -2,13 +2,14 @@
 //
 // v0.1 - initial relase, only suppors 1 psx controller
 // v0.2 - updated relase, supports 2 psx controllers
+// v0.3 - updated relase, supports 2 psx controllers, using 2 joystick implimentations
 //
 // NOTE: This sketch file is for use with Arduino Leonardo and
 //       Arduino Micro only.
 //
 // by John Tryba
 // 9/23/2019
-// v0.2
+// v0.3
 //--------------------------------------------------------------------
 
 #define PSX_LEFT      0
@@ -25,6 +26,9 @@
 #define PSX_L1        11
 #define PSX_R2        12
 #define PSX_L2        13
+
+#define JOYSTICK_COUNT 2
+#define BUTTON_COUNT 14
 
 #include <Joystick.h>                                     // Includes the Joystick Library
 
@@ -47,13 +51,22 @@
 
 #define debugPin A1
 
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
-                   28, 0,                  // Button Count, Hat Switch Count
+Joystick_ Joystick[JOYSTICK_COUNT] = {
+  
+  Joystick_(0x03, JOYSTICK_TYPE_GAMEPAD,
+                   14, 0,                  // Button Count, Hat Switch Count
                    false, false, false,    // No X, Y, and Z Axis
                    false, false, false,    // No Rx, Ry, or Rz
                    false, false,           // No rudder or throttle
-                   false, false, false);   // No accelerator, brake, or steering
+                   false, false, false),   // No accelerator, brake, or steering
 
+  Joystick_(0x04, JOYSTICK_TYPE_GAMEPAD,
+                   14, 0,                  // Button Count, Hat Switch Count
+                   false, false, false,    // No X, Y, and Z Axis
+                   false, false, false,    // No Rx, Ry, or Rz
+                   false, false,           // No rudder or throttle
+                   false, false, false)   // No accelerator, brake, or steering
+};
 
 
 Psx Psx0;                                                  // Initializes the library for player 1
@@ -61,8 +74,7 @@ Psx Psx1;                                                  // Initializes the li
 
 byte button[14] = {0, 1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
-unsigned int data0 = 0;                                    // data stores the controller response for player 1
-unsigned int data1 = 0;                                    // data stores the controller response for player 2
+unsigned int data[2];                                    // data stores the controller response
 byte debug = 0x00;                                        // data stores the debug value
 
 void setup() {
@@ -72,17 +84,12 @@ void setup() {
   // measured in microseconds.
   // too small delay may not work (under 5)
 
-  Psx1.setupPins(dataPin1, cmndPin1, attPin1, clockPin1, psxDelay); // Defines what each pin is used
-  // (Data Pin #, Cmnd Pin #, Att Pin #, Clk Pin #, Delay)
-  // Delay measures how long the clock remains at each state,
-  // measured in microseconds.
-  // too small delay may not work (under 5)
+  Psx1.setupPins(dataPin1, cmndPin1, attPin1, clockPin1, psxDelay);
 
-  data0 = Psx0.read();                                      // Psx.read() initiates the PSX controller and returns
+  data[0] = Psx0.read();                                      // Psx.read() initiates the PSX controller and returns
   // the button data
 
-  data1 = Psx1.read();                                      // Psx.read() initiates the PSX controller and returns
-  // the button data
+  data[1] = Psx1.read();
 
 
   pinMode(ackPinP0, INPUT_PULLUP);                          // ACK Acknowledge signal from Controller to PSX.
@@ -92,11 +99,7 @@ void setup() {
   // the PSX will then start interogating other devices.
 
 
-  pinMode(ackPinP1, INPUT_PULLUP);                          // ACK Acknowledge signal from Controller to PSX.
-  // This signal should go low for at least one clock period
-  // after each 8 bits are sent and ATT is still held low.
-  // If the ACK signal does not go low within about 60 us
-  // the PSX will then start interogating other devices.
+  pinMode(ackPinP1, INPUT_PULLUP);
 
   pinMode(debugPin, INPUT_PULLUP);
   
@@ -106,63 +109,65 @@ void setup() {
   }
   
   // Initialize Joystick Library
-  Joystick.begin();
-
+  for (int index = 0; index < JOYSTICK_COUNT; index++) {
+    Joystick[index].begin();
+  }
 }
 
-byte lastButtonState[28] = {
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+byte lastButtonState[2][BUTTON_COUNT] = {
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 };
 
 void loop() {
 
-  data0 = Psx0.read();
-  data1 = Psx1.read();
+  data[0] = Psx0.read();
+  data[1] = Psx1.read();
 
   if (debug == 0xFF) {
     char buf[64];
     sprintf(buf, "0x%d%d%d%d%d%d%d%d%d%d%d%d%d%d 0x%d%d%d%d%d%d%d%d%d%d%d%d%d%d",
-            bitRead(data0, button[PSX_LEFT]),
-            bitRead(data0, button[PSX_DOWN]),
-            bitRead(data0, button[PSX_RIGHT]),
-            bitRead(data0, button[PSX_UP]),
-            bitRead(data0, button[PSX_START]),
-            bitRead(data0, button[PSX_SELECT]),
-            bitRead(data0, button[PSX_SQUARE]),
-            bitRead(data0, button[PSX_X]),
-            bitRead(data0, button[PSX_O]),
-            bitRead(data0, button[PSX_TRIANGE]),
-            bitRead(data0, button[PSX_R1]),
-            bitRead(data0, button[PSX_L1]),
-            bitRead(data0, button[PSX_R2]),
-            bitRead(data0, button[PSX_L2]),
-            bitRead(data1, button[PSX_LEFT]),
-            bitRead(data1, button[PSX_DOWN]),
-            bitRead(data1, button[PSX_RIGHT]),
-            bitRead(data1, button[PSX_UP]),
-            bitRead(data1, button[PSX_START]),
-            bitRead(data1, button[PSX_SELECT]),
-            bitRead(data1, button[PSX_SQUARE]),
-            bitRead(data1, button[PSX_X]),
-            bitRead(data1, button[PSX_O]),
-            bitRead(data1, button[PSX_TRIANGE]),
-            bitRead(data1, button[PSX_R1]),
-            bitRead(data1, button[PSX_L1]),
-            bitRead(data1, button[PSX_R2]),
-            bitRead(data1, button[PSX_L2])
+            bitRead(data[0], button[PSX_LEFT]),
+            bitRead(data[0], button[PSX_DOWN]),
+            bitRead(data[0], button[PSX_RIGHT]),
+            bitRead(data[0], button[PSX_UP]),
+            bitRead(data[0], button[PSX_START]),
+            bitRead(data[0], button[PSX_SELECT]),
+            bitRead(data[0], button[PSX_SQUARE]),
+            bitRead(data[0], button[PSX_X]),
+            bitRead(data[0], button[PSX_O]),
+            bitRead(data[0], button[PSX_TRIANGE]),
+            bitRead(data[0], button[PSX_R1]),
+            bitRead(data[0], button[PSX_L1]),
+            bitRead(data[0], button[PSX_R2]),
+            bitRead(data[0], button[PSX_L2]),
+            bitRead(data[1], button[PSX_LEFT]),
+            bitRead(data[1], button[PSX_DOWN]),
+            bitRead(data[1], button[PSX_RIGHT]),
+            bitRead(data[1], button[PSX_UP]),
+            bitRead(data[1], button[PSX_START]),
+            bitRead(data[1], button[PSX_SELECT]),
+            bitRead(data[1], button[PSX_SQUARE]),
+            bitRead(data[1], button[PSX_X]),
+            bitRead(data[1], button[PSX_O]),
+            bitRead(data[1], button[PSX_TRIANGE]),
+            bitRead(data[1], button[PSX_R1]),
+            bitRead(data[1], button[PSX_L1]),
+            bitRead(data[1], button[PSX_R2]),
+            bitRead(data[1], button[PSX_L2])
            );
     Serial.println(buf);                                   // Display the returned value
   }
 
   // Read psx btn values
-  for (int index = 0; index < 28; index++)
-  {
-    byte currentButtonState = bitRead(index < 14 ? data0 : data1, button[index < 14 ? index : index - 14]);
-    if (currentButtonState != lastButtonState[index])
-    {
-      Joystick.setButton(index, currentButtonState);
-      lastButtonState[index] = currentButtonState;
+  for (int joyIndex = 0; joyIndex < JOYSTICK_COUNT; joyIndex++) {
+    
+    for (int buttonIndex = 0; buttonIndex < BUTTON_COUNT; buttonIndex++) {
+      byte currentButtonState = bitRead(data[joyIndex], button[buttonIndex]);
+      if (currentButtonState != lastButtonState[joyIndex][buttonIndex]) {
+        Joystick[joyIndex].setButton(buttonIndex, currentButtonState);
+        lastButtonState[joyIndex][buttonIndex] = currentButtonState;
+      }
     }
   }
 
